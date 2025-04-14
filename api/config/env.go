53727c4455f10f6470d692a2487e2e3e4b663b51
Atul Patil <atuluttampatil.1757@gmail.com>
2025-04-14 21:55:47 +0530

    ci: add health check for the container

--- api/config/env.go
+++ api/config/env.go
@@ -2,13 +2,15 @@ package config
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func init() {
-
-	if err := godotenv.Load(".env.local"); err != nil {
-		fmt.Println("ERROR: loading env files")
+	if os.Getenv("GIN_MODE") != "release" {
+		if err := godotenv.Load(".env.local"); err != nil {
+			fmt.Println("ERROR: loading env files", err)
+		}
 	}
 }
